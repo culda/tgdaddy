fe9fdc6dca4c2bb9537342d3257f49970f21d93e
Bogdan Culda <culda@proton.me>
2023-12-17 19:03:20 +0000

    add ga tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-C4D1L2WWV5"
+        ></Script>
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag() {
+                window.dataLayer.push(arguments);
+              }
+              gtag("js", new Date());
+              gtag("config", "G-C4D1L2WWV5");
+            `,
+          }}
+        ></Script>
+      </head>
       <body className={inter.className}>
         <SessionProvider>{children}</SessionProvider>
       </body>
--- react.d.ts
+++ react.d.ts
@@ -1,8 +1,16 @@
+export {};
+
 declare namespace React {
   interface HTMLAttributes<T> {
     class?: string;
   }
   interface SVGAttributes<T> {
     class?: string;
+  }
+}
+
+declare global {
+  interface Window {
+    dataLayer: Record<string, any>[];
   }
 }
