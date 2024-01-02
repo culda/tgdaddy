32b9f75f30cfd783f9a50e1689f0358af5ea5d5f
Bogdan Culda <culda@proton.me>
2024-01-02 13:04:03 +0000

    video and pricing

--- app/page.tsx
+++ app/page.tsx
@@ -48,7 +48,7 @@ export default function Home() {
         {/* Enter page name */}
         <section className="text-gray-600 body-font flex items-center justify-center">
           <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-            <div className=" lg:pl-24 md:pl-16 flex flex-col sm:items-start sm:text-left items-center text-center">
+            <div className=" md:pl-24 md:pl-16 flex flex-col sm:items-start sm:text-left items-center text-center">
               <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
                 Monetize your Telegram audience in under 2 minutes
               </h1>
@@ -110,18 +110,23 @@ export default function Home() {
 
         <section className="text-gray-600 body-font">
           <div className="container px-5 py-24 mx-auto flex flex-wrap">
-            <div className="lg:w-1/2 w-full mb-10 lg:mb-0 rounded-lg overflow-hidden">
+            <div className="w-full md:w-1/2 mb-10 md:mb-0 pb-[120%] md:pb-[60%] rounded-lg overflow-hidden relative">
               <iframe
-                // style="border:0;"
-                width="600"
-                height="850"
-                src="https://www.tella.tv/video/clqgqe03500000hl504bmdfwt/embed?b=0&title=0&a=1&loop=0&t=0&muted=0"
+                style={{
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                  width: '100%',
+                  height: '100%',
+                  border: 0,
+                }}
+                src="https://www.tella.tv/video/clqw82nup00e60gi6hav9gq8b/embed?b=0&title=0&a=1&loop=0&autoPlay=true&t=0&muted=1"
                 allowFullScreen
                 allowTransparency
               ></iframe>
             </div>
-            <div className="flex flex-col flex-wrap lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center">
-              <div className="flex flex-col mb-10 lg:items-start items-center">
+            <div className="flex flex-col flex-wrap md:py-6 -mb-10 md:w-1/2 md:pl-12 md:text-left text-center">
+              <div className="flex flex-col mb-10 md:items-start items-center">
                 <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
                   <FaRocket className="text-xl" />
                 </div>
@@ -135,7 +140,7 @@ export default function Home() {
                   </p>
                 </div>
               </div>
-              <div className="flex flex-col mb-10 lg:items-start items-center">
+              <div className="flex flex-col mb-10 md:items-start items-center">
                 <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
                   <FaLightbulb className="text-xl" />
                 </div>
@@ -149,7 +154,7 @@ export default function Home() {
                   </p>
                 </div>
               </div>
-              <div className="flex flex-col mb-10 lg:items-start items-center">
+              <div className="flex flex-col mb-10 md:items-start items-center">
                 <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
                   <FaMoneyBill className="text-xl" />
                 </div>
@@ -161,6 +166,251 @@ export default function Home() {
                     Allow other creators to post ads on your page and earn
                     commission
                   </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        {/* Pricing */}
+        <section className="text-gray-600 body-font overflow-hidden">
+          <div className="container px-5 py-24 mx-auto">
+            <div className="flex flex-col text-center w-full mb-20">
+              <h1 className="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">
+                Pricing
+              </h1>
+              <p className="md:w-2/3 mx-auto leading-relaxed text-base text-gray-500">
+                The best pricing for you depends on the number of monthly
+                subscribers your page has. Don't worry, you can change it later.
+              </p>
+            </div>
+            <div className="flex flex-wrap -m-4">
+              <div className="p-4 xl:w-1/4 md:w-1/2 w-full">
+                <div className="h-full p-6 rounded-lg border-2 border-gray-300 flex flex-col relative overflow-hidden">
+                  <h2 className="text-sm tracking-widest title-font mb-1 font-medium">
+                    Starter
+                  </h2>
+                  <h1 className="text-5xl text-gray-900 pb-4 mb-4 border-b border-gray-200 leading-none">
+                    Free
+                  </h1>
+                  <p className="flex items-center text-gray-600 mb-2">
+                    <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2.5"
+                        className="w-3 h-3"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M20 6L9 17l-5-5"></path>
+                      </svg>
+                    </span>
+                    18% transaction fee
+                  </p>
+
+                  <Button className="flex-grow-0" onClick={getStarted}>
+                    Get started
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      className="w-4 h-4 ml-auto"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M5 12h14M12 5l7 7-7 7"></path>
+                    </svg>
+                  </Button>
+                  <p className="text-xs text-gray-500 mt-3"></p>
+                </div>
+              </div>
+              <div className="p-4 xl:w-1/4 md:w-1/2 w-full">
+                <div className="h-full p-6 rounded-lg border-2 border-gray-300 flex flex-col relative overflow-hidden">
+                  <h2 className="text-sm tracking-widest title-font mb-1 font-medium">
+                    Growth
+                  </h2>
+                  <h1 className="text-5xl text-gray-900 leading-none flex items-center pb-4 mb-4 border-b border-gray-200">
+                    <span>$49</span>
+                    <span className="text-lg ml-1 font-normal text-gray-500">
+                      /mo
+                    </span>
+                  </h1>
+                  <p className="flex items-center text-gray-600 mb-2">
+                    <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2.5"
+                        className="w-3 h-3"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M20 6L9 17l-5-5"></path>
+                      </svg>
+                    </span>
+                    8% transaction fee
+                  </p>
+
+                  <p className="flex items-center text-gray-600 mb-2">
+                    <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2.5"
+                        className="w-3 h-3"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M20 6L9 17l-5-5"></path>
+                      </svg>
+                    </span>
+                    Best for $500+/mo pages
+                  </p>
+                  <Button className="flex-grow-0" onClick={getStarted}>
+                    Get started
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      className="w-4 h-4 ml-auto"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M5 12h14M12 5l7 7-7 7"></path>
+                    </svg>
+                  </Button>
+                  <p className="text-xs text-gray-500 mt-3"></p>
+                </div>
+              </div>
+              <div className="p-4 xl:w-1/4 md:w-1/2 w-full">
+                <div className="h-full p-6 rounded-lg border-2 border-indigo-500 flex flex-col relative overflow-hidden">
+                  <span className="bg-indigo-500 text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl">
+                    POPULAR
+                  </span>
+                  <h2 className="text-sm tracking-widest title-font mb-1 font-medium">
+                    Pro
+                  </h2>
+                  <h1 className="text-5xl text-gray-900 leading-none flex items-center pb-4 mb-4 border-b border-gray-200">
+                    <span>$99</span>
+                    <span className="text-lg ml-1 font-normal text-gray-500">
+                      /mo
+                    </span>
+                  </h1>
+                  <p className="flex items-center text-gray-600 mb-2">
+                    <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2.5"
+                        className="w-3 h-3"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M20 6L9 17l-5-5"></path>
+                      </svg>
+                    </span>
+                    5% transaction fee
+                  </p>
+                  <p className="flex items-center text-gray-600 mb-2">
+                    <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2.5"
+                        className="w-3 h-3"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M20 6L9 17l-5-5"></path>
+                      </svg>
+                    </span>
+                    Best for $1000+/mo pages
+                  </p>
+                  <Button className="flex-grow-0" onClick={getStarted}>
+                    Get started
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      className="w-4 h-4 ml-auto"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M5 12h14M12 5l7 7-7 7"></path>
+                    </svg>
+                  </Button>
+                  <p className="text-xs text-gray-500 mt-3"></p>
+                </div>
+              </div>
+              <div className="p-4 xl:w-1/4 md:w-1/2 w-full">
+                <div className="h-full p-6 rounded-lg border-2 border-gray-300 flex flex-col relative overflow-hidden">
+                  <h2 className="text-sm tracking-widest title-font mb-1 font-medium">
+                    Business
+                  </h2>
+                  <h1 className="text-5xl text-gray-900 leading-none flex items-center pb-4 mb-4 border-b border-gray-200">
+                    <span>$199</span>
+                    <span className="text-lg ml-1 font-normal text-gray-500">
+                      /mo
+                    </span>
+                  </h1>
+                  <p className="flex items-center text-gray-600 mb-2">
+                    <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2.5"
+                        className="w-3 h-3"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M20 6L9 17l-5-5"></path>
+                      </svg>
+                    </span>
+                    1% transaction fee
+                  </p>
+
+                  <p className="flex items-center text-gray-600 mb-2">
+                    <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2.5"
+                        className="w-3 h-3"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M20 6L9 17l-5-5"></path>
+                      </svg>
+                    </span>
+                    Best for $2000+/mo pages
+                  </p>
+
+                  <Button className="flex-grow-0" onClick={getStarted}>
+                    Get started
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      className="w-4 h-4 ml-auto"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M5 12h14M12 5l7 7-7 7"></path>
+                    </svg>
+                  </Button>
+                  <p className="text-xs text-gray-500 mt-3"></p>
                 </div>
               </div>
             </div>
