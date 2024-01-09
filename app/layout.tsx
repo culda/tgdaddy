c821cac7b2477ba584fcbb9d6c362e4b32d232c9
Bogdan Culda <culda@proton.me>
2024-01-09 18:48:33 +0000

    rm ga tag in favor of gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,18 +23,6 @@ export default function RootLayout({
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-C4D1L2WWV5"
         ></Script>
-        <Script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag() {
-                window.dataLayer.push(arguments);
-              }
-              gtag("js", new Date());
-              gtag("config", "G-C4D1L2WWV5");
-            `,
-          }}
-        ></Script>
       </head>
       <body className={alatsi.className}>
         <SnackbarProvider>
